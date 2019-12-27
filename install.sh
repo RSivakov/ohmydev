6dd6f6e1cc9b303158348ae80dcb367e8415d133
Roman Sivakov <romansivakov@gmail.com>
2019-12-27 10:17:16 +0300

    Добавлены инструкции npm и папка bin
    Signed-off-by: Roman Sivakov <romansivakov@gmail.com>

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# Created by Roman Sivakov <romansivakov@gmail.com> for project OhMyDev
+
+
+echo "export 'PATH=$HOME/Code/ohmydev/bin:$PATH" >> ~/.zshrc
+source ~/.zshrc
---
+++ npm.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# Created by Roman Sivakov <romansivakov@gmail.com> for project OhMyDev
+
+npm install -g laravel-echo-server
