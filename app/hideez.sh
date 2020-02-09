0a102a6a35d31f4a21d02a890e14117263ed75ad
Roman Sivakov <romansivakov@gmail.com>
2020-02-09 20:09:15 +0300

    добавлена тема Cobalt 2, Env штуки и Brewfile

--- app/hideez.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Created by Roman Sivakov <romansivakov@gmail.com> for project OhMyDev
-
-#Установка Hideez Sаfe
-# wget https://hideez.com/static/files/HideezSafe.dmg >> ~/Downloads/
-# open ~/Downloads/HideezSafe.dmg
-# cp -R /Volumes/Hideez\ Safe/Hideez\ Safe.app /Applications/
-# nohup /Applications/Hideez\ Safe.app/Contents/MacOS/Hideez\ Safe start.sh &
